ff4ccb991eaff1d0f907485ad6b4cf4d6b520c36
Syafiq Faiz <syafiqfaiz@gmail.com>
2023-07-19 12:46:32 +0800

    basic without hotwire

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
+# == Route Map
+#
+
 Rails.application.routes.draw do
+  resources :rooms do
+    resources :messages
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "rooms#index"
 end
